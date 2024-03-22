f42de41f0f3830233dca9133ce2d32221f11b94b
erfect2020 <94505384+erfect2020@users.noreply.github.com>
2024-03-22 16:01:23 +0800

    Update contrast_loss.py

--- loss/contrast_loss.py
+++ loss/contrast_loss.py
@@ -1,24 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import MSELoss, L1Loss
-from loss.InfoNCE import InfoNCE
 import einops
-from loss.generate_index import compute_index_mat
-
-
-class contrastiveLoss(nn.Module):
-    def __init__(self):
-        super(contrastiveLoss, self).__init__()
-        self.infonce = InfoNCE()
-
-    def forward(self, query, positive_key, negative_keys):
-        losses = {}
-        query = einops.rearrange(query, 'b c p -> (b c) p')
-        positive_key = einops.rearrange(positive_key, 'b c p -> (b c) p')
-        negative_keys = einops.rearrange(negative_keys, 'b n c p -> (b c n) p')
-        output = self.infonce(query, positive_key, negative_keys)
-        losses["total_loss"] = output
-        return losses
 
 def chunk_cosine_sim(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     """ Computes cosine similarity between all possible pairs in two sets of vectors.
