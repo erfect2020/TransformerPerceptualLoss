0d33cd3a930a8902398316607bf600388a836deb
erfect2020 <94505384+erfect2020@users.noreply.github.com>
2024-03-22 20:26:14 +0800

    Update deblur_loss.py

--- loss/deblur_loss.py
+++ loss/deblur_loss.py
@@ -15,52 +15,11 @@ from loss.vgg_loss import PerceptualVGGLoss
 from utils.pos_embed import interpolate_pos_embed, interpolate_pos_encoding
 
 
-class ReconstructLoss(nn.Module):
-    def __init__(self, opt):
-        super(ReconstructLoss, self).__init__()
-        self.l1_loss = L1Loss(reduction='mean')
-
-        img_size = opt['image_size']
-        pretrained_ckpt = opt['pretrain_mae']
-        self.pretrain_mae = mae_vit_base_patch16(img_size=img_size)
-        pretrained_ckpt = os.path.expanduser(pretrained_ckpt)
-        checkpoint = torch.load(pretrained_ckpt, map_location='cpu')
-        print("Load pre-trained checkpoint from: %s" % pretrained_ckpt)
-        checkpoint_model = checkpoint['model']
-        state_dict = self.pretrain_mae.state_dict()
-        for k in ['head.weight', 'head.bias']:
-            if k in checkpoint_model and checkpoint_model[k].shape != state_dict[k].shape:
-                print(f"Removing key {k} from pretrained checkpoint")
-                del checkpoint_model[k]
-        # interpolate position embedding
-        interpolate_pos_embed(self.pretrain_mae, checkpoint_model)
-        self.pretrain_mae.load_state_dict(checkpoint_model, strict=False)
-        for _, p in self.pretrain_mae.named_parameters():
-            p.requires_grad = False
-        self.perceptual_loss = selfPerceptualLoss(img_size)
-
-
-    def forward(self, recover_img, gt):
-        losses = {}
-        loss_l1 = self.l1_loss(recover_img, gt)
-        with torch.no_grad():
-            predict_embed, _ = self.pretrain_mae(recover_img)
-            gt_embed, _ = self.pretrain_mae(gt)
-        contrast_loss = self.perceptual_loss(predict_embed, gt_embed)
-        contrast_loss = contrast_loss * 0.1
-        losses["l1"] = loss_l1
-        losses["Perceptual"] = contrast_loss
-        losses["total_loss"] = loss_l1 + contrast_loss
-        return losses
-
-
-
 class ReconstructPerceptualLoss(nn.Module):
     def __init__(self, opt):
         super(ReconstructPerceptualLoss, self).__init__()
         self.l1_loss = L1Loss(reduction='mean')
         self.mse_loss = MSELoss(reduction='mean')
-        self.hist_loss = HistLoss()
 
         img_size = opt['image_size']
         pretrained_ckpt = opt['pretrain_mae']
@@ -92,15 +51,16 @@ class ReconstructPerceptualLoss(nn.Module):
         recover_img = normalize(recover_img, self.normalize_mean, self.normalize_std)
         gt = normalize(gt, self.normalize_mean, self.normalize_std)
         predict_embed, gt_embed, ids = self.pretrain_mae(recover_img, gt, 0.50)
-        # contrast_loss = self.constrastive_loss(predict_embed, gt_embed, ids) * 0.1
+        # Local MAE
+        contrast_loss = self.constrastive_loss(predict_embed, gt_embed, ids) * 0.1
 
-        contrast_loss =0
-        for predict_e, gt_e in zip(predict_embed, gt_embed):
-            contrast_loss += projectedDistributionLoss(predict_e, gt_e) * 1e-4
+        # Global MAE
+        # contrast_loss =0
+        # for predict_e, gt_e in zip(predict_embed, gt_embed):
+        #    contrast_loss += projectedDistributionLoss(predict_e, gt_e) * 1e-4
 
         losses["l1"] = loss_l1
         losses["Perceptual"] = contrast_loss
-
         losses["total_loss"] = contrast_loss + loss_l1 
 
 
@@ -136,8 +96,7 @@ class ReconstructVitPerceptualLoss(nn.Module):
 
     def forward(self, recover_img, gt):
         losses = {}
-        loss_l2 = self.mse_loss(recover_img, gt)
-
+        loss_l1 = self.l1_loss(recover_img, gt)
 
         recover_img = normalize(recover_img, self.normalize_mean, self.normalize_std)
         gt = normalize(gt, self.normalize_mean, self.normalize_std)
@@ -146,9 +105,9 @@ class ReconstructVitPerceptualLoss(nn.Module):
         gt_embed = self.pretrain_mae(gt)
         contrast_loss = self.perceptual_loss(predict_embed, gt_embed, None)
         contrast_loss =  0.003 * contrast_loss
-        losses["l2"] = loss_l2
+        losses["l1"] = loss_l1
         losses["Perceptual"] = contrast_loss
-        losses["total_loss"] = contrast_loss + loss_l2 
+        losses["total_loss"] = contrast_loss + loss_l1
 
         return losses
 
@@ -157,7 +116,6 @@ class ReconstructDINOPerceptualLoss(nn.Module):
         super(ReconstructDINOPerceptualLoss, self).__init__()
         self.l1_loss = L1Loss(reduction='mean')
         self.mse_loss = MSELoss(reduction='mean')
-        self.hist_loss = HistLoss()
 
         self.pretrain_mae = vit_base_patch16_224(pretrained=True)
 
@@ -194,7 +152,7 @@ class ReconstructDINOPerceptualLoss(nn.Module):
 
     def forward(self, recover_img, gt):
         losses = {}
-        loss_l2 = self.mse_loss(recover_img, gt)
+        loss_l1 = self.l1_loss(recover_img, gt)
 
         recover_img = normalize(recover_img, self.normalize_mean, self.normalize_std)
         gt = normalize(gt, self.normalize_mean, self.normalize_std)
@@ -203,8 +161,8 @@ class ReconstructDINOPerceptualLoss(nn.Module):
         gt_embed = self.pretrain_mae(gt)
         contrast_loss = self.perceptual_loss(predict_embed, gt_embed, None)
         contrast_loss = 0.01 * contrast_loss
-        losses["l2"] = loss_l2
+        losses["l1"] = loss_l1
         losses["Perceptual"] = contrast_loss
-        losses["total_loss"] = contrast_loss + loss_l2 
+        losses["total_loss"] = contrast_loss + loss_l1 
 
         return losses
